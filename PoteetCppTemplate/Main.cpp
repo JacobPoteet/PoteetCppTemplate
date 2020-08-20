5c4fe79e789913d1a32cc15c2702bc01dd998802
JacobPoteet <64765148+gwlpotassium@users.noreply.github.com>
2020-08-19 23:05:37 -0500

    initial check in

---
+++ PoteetCppTemplate/Main.cpp
@@ -0,0 +1,25 @@
+//Jacob Poteet
+//2020
+//Austin, TX
+#include "Main.h"
+
+
+int main() {
+
+    //mainFile.open(FileToSearch);
+    //mainFile.close();
+
+
+
+    PauseCommandPrompt();
+    return 0;
+}
+
+
+
+void PrintString(string message) {
+
+    cout << message << endl;
+
+    return;
+}
---
+++ PoteetCppTemplate/Main.h
@@ -0,0 +1,36 @@
+//Jacob Poteet
+
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+//#include <fstream>
+
+using namespace std;
+
+
+
+/*-----------------------------------*/
+/*Global Variable*/
+/*-----------------------------------*/
+
+//string FileToSearch = "MainDoc.txt";
+//ifstream mainFile;
+
+/*-----------------------------------*/
+
+
+
+
+/*-----------------------------------*/
+/*Functions*/
+/*-----------------------------------*/
+
+//keep command prompt active
+void PauseCommandPrompt() { system("pause"); };
+
+
+//debug print
+void PrintString(string message);
+
+/*-----------------------------------*/
